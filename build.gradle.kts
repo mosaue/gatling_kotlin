02cc15af5cee7e0abacd94c1f8e12b8d746ea3b8
Morten Saue <morten.saue@ruter.no>
2022-11-22 13:49:07 +0100

    init-state

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,56 +26,6 @@ gatling {
 
 }
 
-val spec = "http://192.168.1.93:8080/bank/v2/api-docs"
-val generatedSources = "$buildDir/generated/bank"
-
-sourceSets {
-    getByName("main") {
-        java {
-            srcDir("$generatedSources/src/main/kotlin")
-        }
-    }
-}
-
-tasks{
-    openApiGenerate{
-        generatorName.set("kotlin")
-        inputSpec.set(spec)
-        outputDir.set(generatedSources)
-
-        skipValidateSpec.set(true)
-
-        modelPackage.set("com.example.gatling.api")
-        modelPackage.set("com.example.gatling.model")
-
-        systemProperties.set(
-            mapOf(
-                "models" to "", // Only generate models (not the api and supporting files)
-                "modelDocs" to "false"
-            )
-        )
-
-        configOptions.set(
-            mapOf(
-                "dateLibrary" to "java8",
-                "enumPropertyNaming" to "PascalCase",
-                "serializationLibrary" to "jackson"
-            )
-        )
-
-        typeMappings.set(
-            mapOf(
-                "ByteArray" to "kotlin.String",
-                "java.time.OffsetDateTime" to "kotlin.String"
-            )
-        )
-    }
-
-    compileKotlin {
-        dependsOn(openApiGenerate)
-    }
-}
-
 tasks.test {
     useJUnitPlatform()
 }

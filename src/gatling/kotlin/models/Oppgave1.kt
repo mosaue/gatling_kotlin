00e93627f10c6412271814ff5a59e60f98f4076b
Morten Saue <morten.saue@ruter.no>
2022-11-22 15:44:33 +0100

    work

--- src/gatling/kotlin/models/Oppgave1.kt
+++ src/gatling/kotlin/models/Oppgave1.kt
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.apache.commons.lang3.RandomStringUtils
 import java.io.FileWriter
+import java.util.*
+import kotlin.collections.Iterator
 
 //Oppgave 1
 data class User(
@@ -16,12 +18,39 @@ data class User(
     val homePhone: String,
     val lastName: String,
     val locality: String,
-    //TODO: Fyll inn resten av feltene
+    val mobilePhone: String,
+    val postalCode: String,
+    val region: String,
+    val ssn: String,
+    val title: String,
+    val workPhone: String,
+    val emailAddress: String,
+    val password: String
 )
 
 fun main() {
     //Oppgave 2
-    val feeder: Map<String, User> = TODO("Fullfør sekvensgeneratoren")
+    val feeder: Iterator<Map<String, User>> = generateSequence {
+        val user = User(
+            address = "string",
+            country = "string",
+            dob = "12/12/1990",
+            firstName = "string",
+            gender = "M",
+            homePhone = "(111) 111-1111",
+            lastName = "string",
+            locality = "string",
+            mobilePhone = "(111) 111-1111",
+            postalCode = "string",
+            region = "string",
+            ssn = "${Random().nextInt(111, 999)}-${Random().nextInt(11, 99)}-${Random().nextInt(1111, 9999)}",
+            title = "Mr.",
+            workPhone = "(111) 111-1111",
+            emailAddress = RandomStringUtils.randomAlphanumeric(20) + "@foo.com",
+            password = "${RandomStringUtils.randomAlphabetic(16)}aB-${Random().nextInt(111, 999)}"
+        )
+        mapOf("user" to user)
+    }.iterator()
 
     //Oppgave 3
     for (i in 0 until 10) {
